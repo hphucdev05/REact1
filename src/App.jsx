53d83913acd815c3fd06d424b42cdacf774e2d04
Le Hoang Phuc <hoangphucdev05>
2024-09-20 12:06:50 +0700

    video29

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import MyComponents from './learn/MyComponents'
 import SecondComponent from './learn/SecondComponent'
 // () => { }
 const App = () => {
@@ -21,6 +21,7 @@ const App = () => {
       <h1>Hello world
 
         <SecondComponent />
+        <MyComponents />
 
       </h1>
       <div className="card">
