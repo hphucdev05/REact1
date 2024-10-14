09153529b833f8808fd862091294ff3b32deca63
Le Hoang Phuc <hoangphucdev05>
2024-10-14 15:32:14 +0700

    video31

--- src/learn/MyComponents.jsx
+++ src/learn/MyComponents.jsx
@@ -1,13 +1,24 @@
 import './style.css'
 const Mycomponent = () => {
+    // let word = "Hello Phuc!";
+    // const word = "video 31";
+    // const bool = true;
+    // const bool = undefined
+    // const check = null
+    const obj = [1, 2, 3];
+    // const obj = {
+    //     name: "Phuc",
+    //     age: 19
+    // }
     return (
         <>
-            <div >video30</div>
+            <div > {JSON.stringify(obj)}</div>
+
             <div className="child"
                 style={
                     { borderRadius: "10px" }
                 }
-            > child</div>
+            > fkinghard</div>
         </>
     )
 }
