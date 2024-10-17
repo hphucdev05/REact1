87b85ae9f51e359d333a6a63be2d05aa9ef717c9
Le Hoang Phuc <hoangphucdev05>
2024-10-17 20:27:23 +0700

    video35

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import TodoNew from './components/todo/todoNew';
 import './components/todo/todo.css';
 import reactLogo from './assets/react.svg';
 
+
 const App = () => {
   const name = "phuc";
   const age = 25;
@@ -11,14 +12,23 @@ const App = () => {
     country: "Viet Nam"
 
   }
+  const addNewtodo = (name) => {
+    alert(`call me ${name}`)
+
+  }
+
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
-      <TodoNew />
+      <TodoNew
+        addNewtodo={addNewtodo}
+      />
       <TodoData
         name={name}
         age={age}
         data={data}
+
       />
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -1,5 +1,8 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log("check props>>>", props)
+    const { addNewtodo } = props;
+    addNewtodo("phuchoang")
     return (
         <div className='todo-new'>
             <input type="text" />
