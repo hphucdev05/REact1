548971b1f3ba316f089a8f33da5bb194a8883947
Le Hoang Phuc <hoangphucdev05>
2024-10-17 16:21:37 +0700

    video29

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import TodoData from './components/todo/todoData';
 import TodoNew from './components/todo/todoNew';
 import './components/todo/todo.css';
@@ -10,17 +9,6 @@ const App = () => {
   const data = {
     address: "Ho Chi Minh",
     country: "Viet Nam"
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponents from './learn/MyComponents'
-import SecondComponent from './learn/SecondComponent'
-// () => { }
-const App = () => {
-  const [count, setCount] = useState(0)
->>>>>>> 09153529b833f8808fd862091294ff3b32deca63
 
   }
   return (
@@ -35,28 +23,7 @@ const App = () => {
       <div className='todo-image'>
         <img src={reactLogo} className='logo' />
       </div>
-<<<<<<< HEAD
     </div>
-=======
-      <h1>Hello world
-
-        <SecondComponent />
-        <MyComponents />
-
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
->>>>>>> 09153529b833f8808fd862091294ff3b32deca63
   )
 }
 
