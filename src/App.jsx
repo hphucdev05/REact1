f6a463b5cc4e13daaef3ecd5475de6c25e7c841b
Le Hoang Phuc <hoangphucdev05>
2024-11-06 16:45:59 +0700

    video34

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import './components/todo/todoData'
 import TodoData from './components/todo/todoData'
 import TodoNew from './components/todo/todoNew'
 import reactlogo from './assets/react.svg'
+const name = "Hoang Phuc"
+const age = 25
+const data = {
+  address: "ha noi",
+  country: "viet nam"
+}
 const App = () => {
   return (
     <div className='todo-container'>
       <div className='todo-title' >Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={name}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
         <img src={reactlogo} className='logo' />
       </div>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,7 +1,11 @@
 const TodoData = (props) => {
     console.log('check props>>>>: ', props)
+    const { name, age, data } = props
     return (
         <div className='todo-data'>
+            <div>my name is: {name}</div>
+            <div>my age is: {age}</div>
+
             <div>Learning react</div>
             <div>Watch youtube</div>
         </div>
