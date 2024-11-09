11e5f036ebcb9fe6dc440c2aedd66740e2fdea59
Le Hoang Phuc <hoangphucdev05>
2024-11-09 16:22:49 +0700

    video40

--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,23 +1,17 @@
 const TodoData = (props) => {
     const { todoList } = props // lay ra value cua prop
 
-    console.log('check props>>>>: ', todoList)
     return (
         <div className='todo-data'>
             {todoList.map((item, index) => {
-                console.log(">>>>check map: ", item, index)
                 return (
-                    <div className="todo-item">
+                    <div className={`todo-item`} key={item.id}>
                         <div>{item.name}</div>
                         <button>Delete</button>
 
                     </div>
                 )
             })}
-
-            <div>
-                {JSON.stringify(props.todoList)}
-            </div>
         </div>
     )
 }
