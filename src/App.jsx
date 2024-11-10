d8ab0f75ea5fa3377c61e9274c15816054129247
Le Hoang Phuc <hoangphucdev05>
2024-11-10 15:31:43 +0700

    video42

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,18 @@ const App = () => {
       id: randomIntFromInterval(1, 1000000),
       name: name
     }
+
     setTodolist([...todoList, newtodo])// hieu don gian la ...data cu, them data moi vao cuoi data cu
     // array push
-
   }
   const randomIntFromInterval = (min, max) => { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
+
+  }
+  const deletetodo = (id) => {
+    const newtodo = todoList.filter(item => item.id !== id)
+    console.log("check newtodo ", newtodo)
+    setTodolist(newtodo)
   }
   return (
     <div className='todo-container'>
@@ -32,6 +38,7 @@ const App = () => {
       {todoList.length > 0 ?
         <TodoData
           todoList={todoList}
+          deletetodo={deletetodo}
         />
         :
         <div className='todo-image'>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,13 +1,21 @@
 const TodoData = (props) => {
-    const { todoList } = props // lay ra value cua prop
+    const { todoList, deletetodo } = props // lay ra value cua prop
 
+    const handleclick = (id) => {
+        deletetodo(id)
+    }
     return (
         <div className='todo-data'>
             {todoList.map((item, index) => {
                 return (
                     <div className={`todo-item`} key={item.id}>
                         <div>{item.name}</div>
-                        <button>Delete</button>
+                        <button onClick={() => { handleclick(item.id) }}
+                            style={{ cursor: "pointer" }}
+
+                        >
+                            Delete
+                        </button>
 
                     </div>
                 )
